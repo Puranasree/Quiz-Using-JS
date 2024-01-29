98de165228deacc35aa5ad08ea0b6fbed717d4db
Puranasree M S <129767298+Puranasree@users.noreply.github.com>
2024-01-29 08:44:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# quiz using javascript
+
+
---
+++ script.js
@@ -0,0 +1,85 @@
+const quizData = [
+  {
+    question: "Which technology is primarily responsible for the styling of web pages?",
+    a: "JavaScript",
+    b: "HTML",
+    c: "Python",
+    d: "CSS",
+    correct: "d",
+  },
+  {
+    question: "Which programming language is mainly used for adding interactivity to websites?",
+    a: "Python",
+    b: "PHP",
+    c: "Javascript",
+    d: "Ruby",
+    correct: "c",
+  },
+  {
+    question: "Which CSS property is used to change the font size of text?",
+    a: "font-size",
+    b: "font-style",
+    c: "text-style",
+    d: "text-size",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+];
+
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+const deselectAnswers = () => {
+  answerElements.forEach((answer) => (answer.checked = false));
+};
+
+const getSelected = () => {
+  let answer;
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) answer = answerElement.id;
+  });
+  return answer;
+};
+
+const loadQuiz = () => {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+};
+
+loadQuiz();
+
+submitButton.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) score++;
+    currentQuiz++;
+    if (currentQuiz < quizData.length) loadQuiz();
+    else {
+      quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+            <button onclick="history.go(0)">Play Again</button>
+        ` 
+    }
+  }
+});
